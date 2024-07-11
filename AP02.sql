e66704f89d63eee800b3d2ad2efb9c2b2dc7fd42
Gustavo Machado Lupim <90060817+oLupim@users.noreply.github.com>
2024-07-11 19:07:13 -0300

    Update AP02.sql

--- AP02.sql
+++ AP02.sql
@@ -34,3 +34,29 @@ create table participa_de(
     constraint projeto_participa
     foreign key (id_proj) references projeto(id_projeto)
 );
+
+
+INSERT INTO emp_depto (num_depto, nome_depto, num_emp, salario, nome_emp)
+VALUES
+  (1, 'Direcao', 11, 2500, 'Juliano'),
+  (2, 'Gerencia', 12, 3500, 'Julia'),
+  (3, 'Secretaria', 13, 1500, 'Julio');
+
+INSERT INTO dependente (id_dep, nome_dep, data_nasc, numero_emp)
+VALUES
+  (1, 'dep01', 1995, 2),
+  (2, 'dep02', 1993, 2),
+  (3, 'dep03', 1997, 1);
+
+INSERT INTO projeto (id_projeto, nome_projeto, numero_depto)
+VALUES
+  (1, 'projetoADS1', 1995, 3),
+  (2, 'projetoADS2', 1993, 2),
+  (3, 'projetoADS3', 1997, 1);
+
+INSERT INTO participa_de (hs_trab, numero_empdepto, id_proj)
+VALUES
+  (40, 1, 3),
+  (64, 3, 3),
+  (80, 2, 1);
+  
